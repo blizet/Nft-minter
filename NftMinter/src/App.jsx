3c912c0eb37d184c79c65d079e4964b22c0b2a2d
blizet <anjalijha2k3@gmail.com>
2023-07-15 15:40:08 +0530

    first commit

---
+++ NftMinter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import FileUpload from './FileUpload'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Nft Minter</h1>
+     <FileUpload/>
+    </>
+  )
+}
+
+export default App
